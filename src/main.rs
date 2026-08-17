858b420866aea99ad2d0bf298947faa13a306efa
Narngisa <nattanan43658@gmail.com>
2026-08-18 00:26:16 +0700

    Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-mod diff;
-
 fn main() {
 
-    let my_func = |x: f64| (x * x) + (3.0 * x);
-    let result = diff::derivative(my_func, 2.0, 1e-6);
-    println!("Derivative result: {result}");
+    println!("Hello, World !")
 }
